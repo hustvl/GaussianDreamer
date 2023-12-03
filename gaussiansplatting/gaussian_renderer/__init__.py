831035fbe6d42b46998c2b724c01290dc720e875
taoranyi <2201855616@qq.com>
2023-12-03 12:16:32 +0800

    Update __init__.py

--- gaussiansplatting/gaussian_renderer/__init__.py
+++ gaussiansplatting/gaussian_renderer/__init__.py
@@ -45,6 +45,7 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         sh_degree=pc.active_sh_degree,
         campos=viewpoint_camera.camera_center,
         prefiltered=False,
+        debug=False
     )
 
     rasterizer = GaussianRasterizer(raster_settings=raster_settings)
